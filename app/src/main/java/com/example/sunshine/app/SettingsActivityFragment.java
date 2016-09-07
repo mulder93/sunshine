761d9c9761e0eeb90facc8358f46eeabb9386ded
Boris Tsarev <boristsarev@gmail.com>
2016-09-07 23:13:12 +0300

    Added SettingsActivity

---
+++ app/src/main/java/com/example/sunshine/app/SettingsActivityFragment.java
@@ -0,0 +1,20 @@
+package com.example.sunshine.app;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsActivityFragment extends PreferenceFragment
+{
+    public SettingsActivityFragment()
+    {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
